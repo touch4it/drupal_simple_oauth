09439df139e81f24c346d19e0d1db4078b2575fd
Mateu Aguiló Bosch <mateu.aguilo.bosch@gmail.com>
2016-11-26 12:55:44 +0100

    [FEATURE] Test invalid Password grant

--- tests/src/Functional/PasswordFunctionalTest.php
+++ tests/src/Functional/PasswordFunctionalTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Drupal\Tests\simple_oauth\Functional;
+use Drupal\Component\Serialization\Json;
 use Drupal\user\RoleInterface;
 
 /**
@@ -26,24 +27,86 @@ class PasswordFunctionalTest extends TokenBearerFunctionalTestBase {
   protected $path;
 
   /**
-   * Test the GET method.
+   * Test the valid Password grant.
    */
   public function testPasswordGrant() {
+    // 1. Test the valid response.
     $num_roles = mt_rand(1, count($this->additionalRoles));
     $requested_roles = array_slice($this->additionalRoles, 0, $num_roles);
+    $valid_payload = [
+      'grant_type' => 'password',
+      'client_id' => $this->client->uuid(),
+      'client_secret' => $this->clientSecret,
+      'username' => $this->user->getAccountName(),
+      'password' => $this->user->pass_raw,
+      'scope' => implode(' ', array_map(function (RoleInterface $role) {
+        return $role->id();
+      }, $requested_roles)),
+    ];
     $response = $this->request('POST', $this->url, [
-      'form_params' => [
-        'grant_type' => 'password',
-        'client_id' => $this->client->uuid(),
-        'client_secret' => $this->clientSecret,
-        'username' => $this->user->getAccountName(),
-        'password' => $this->user->pass_raw,
-        'scope' => implode(' ', array_map(function (RoleInterface $role) {
-          return $role->id();
-        }, $requested_roles)),
-      ],
+      'form_params' => $valid_payload,
     ]);
     $this->assertValidTokenResponse($response, TRUE);
+
+    // 2. Test the valid without scopes.
+    $payload_no_scope = $valid_payload;
+    unset($payload_no_scope['scope']);
+    $response = $this->request('POST', $this->url, [
+      'form_params' => $payload_no_scope,
+    ]);
+    $this->assertValidTokenResponse($response, TRUE);
+
+  }
+
+  /**
+   * Test invalid Password grant.
+   */
+  public function testInvalidPasswordGrant() {
+    $num_roles = mt_rand(1, count($this->additionalRoles));
+    $requested_roles = array_slice($this->additionalRoles, 0, $num_roles);
+    $valid_payload = [
+      'grant_type' => 'password',
+      'client_id' => $this->client->uuid(),
+      'client_secret' => $this->clientSecret,
+      'username' => $this->user->getAccountName(),
+      'password' => $this->user->pass_raw,
+      'scope' => implode(' ', array_map(function (RoleInterface $role) {
+        return $role->id();
+      }, $requested_roles)),
+    ];
+
+    $data = [
+      'grant_type' => [
+        'error' => 'invalid_grant',
+        'code' => 400,
+      ],
+      'client_id' => [
+        'error' => 'invalid_request',
+        'code' => 400,
+      ],
+      'client_secret' => [
+        'error' => 'invalid_client',
+        'code' => 401,
+      ],
+      'username' => [
+        'error' => 'invalid_request',
+        'code' => 400,
+      ],
+      'password' => [
+        'error' => 'invalid_request',
+        'code' => 400,
+      ],
+    ];
+    foreach ($data as $key => $value) {
+      $invalid_payload = $valid_payload;
+      unset($invalid_payload[$key]);
+      $response = $this->request('POST', $this->url, [
+        'form_params' => $invalid_payload,
+      ]);
+      $parsed_response = Json::decode($response->getBody()->getContents());
+      $this->assertSame($value['code'], $response->getStatusCode());
+      $this->assertSame($value['error'], $parsed_response['error']);
+    }
   }
 
 }
