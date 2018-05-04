092c7298fdf8196c844f1b830f4805a27dbec553
David Ondrus <david.ondrus@touch4it.com>
2018-05-04 08:09:41 +0200

    Applied patch fix for translations in Drupal 8.5.3

--- src/Authentication/TokenAuthUserInterface.php
+++ src/Authentication/TokenAuthUserInterface.php
@@ -2,9 +2,10 @@
 
 namespace Drupal\simple_oauth\Authentication;
 
-use Drupal\user\UserInterface;
+use Drupal\Core\Cache\CacheableDependencyInterface;
+use Drupal\Core\Session\AccountInterface;
 
-interface TokenAuthUserInterface extends \IteratorAggregate, UserInterface {
+interface TokenAuthUserInterface extends \IteratorAggregate, AccountInterface, CacheableDependencyInterface {
 
   /**
    * Get the token.
@@ -13,5 +14,16 @@ interface TokenAuthUserInterface extends \IteratorAggregate, UserInterface {
    *   The provided OAuth2 token.
    */
   public function getToken();
+
+  /**
+   * Whether a user has a certain role.
+   *
+   * @param string $rid
+   *   The role ID to check.
+   *
+   * @return bool
+   *   Returns TRUE if the user has the role, otherwise FALSE.
+   */
+  public function hasRole($rid);
 
 }
